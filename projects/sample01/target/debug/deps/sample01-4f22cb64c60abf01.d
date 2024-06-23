0df0412195681938f2f23af4c7c56b05ecc8ab69
aryalireza <ary.alireza@gmail.com>
2024-06-23 15:04:15 +0330

    add 1st sample changes

---
+++ projects/sample01/target/debug/deps/sample01-4f22cb64c60abf01.d
@@ -0,0 +1,5 @@
+/home/aryalireza/rust/rustpractice/projects/sample01/target/debug/deps/sample01-4f22cb64c60abf01: src/main.rs
+
+/home/aryalireza/rust/rustpractice/projects/sample01/target/debug/deps/sample01-4f22cb64c60abf01.d: src/main.rs
+
+src/main.rs:
---
+++ projects/sample01/target/debug/sample01.d
@@ -0,0 +1,1 @@
+/home/aryalireza/rust/rustpractice/projects/sample01/target/debug/sample01: /home/aryalireza/rust/rustpractice/projects/sample01/src/main.rs
