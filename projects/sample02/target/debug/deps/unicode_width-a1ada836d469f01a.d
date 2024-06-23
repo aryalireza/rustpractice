70bbe1fd82a16a8d55996cd15125ac7336d1f25d
aryalireza <ary.alireza@gmail.com>
2024-06-23 16:46:07 +0330

    add changes in 2nd sample

---
+++ projects/sample02/target/debug/deps/unicode_width-a1ada836d469f01a.d
@@ -0,0 +1,8 @@
+/home/aryalireza/rust/rustpractice/projects/sample02/target/debug/deps/libunicode_width-a1ada836d469f01a.rmeta: /home/aryalireza/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-width-0.1.13/src/lib.rs /home/aryalireza/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-width-0.1.13/src/tables.rs
+
+/home/aryalireza/rust/rustpractice/projects/sample02/target/debug/deps/libunicode_width-a1ada836d469f01a.rlib: /home/aryalireza/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-width-0.1.13/src/lib.rs /home/aryalireza/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-width-0.1.13/src/tables.rs
+
+/home/aryalireza/rust/rustpractice/projects/sample02/target/debug/deps/unicode_width-a1ada836d469f01a.d: /home/aryalireza/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-width-0.1.13/src/lib.rs /home/aryalireza/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-width-0.1.13/src/tables.rs
+
+/home/aryalireza/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-width-0.1.13/src/lib.rs:
+/home/aryalireza/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-width-0.1.13/src/tables.rs:
---
+++ projects/sample02/target/debug/sample02.d
@@ -0,0 +1,1 @@
+/home/aryalireza/rust/rustpractice/projects/sample02/target/debug/sample02: /home/aryalireza/rust/rustpractice/projects/sample02/src/main.rs
